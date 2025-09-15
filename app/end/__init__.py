d1daca73eecd89f6fd7dd89977da234c886930c5
NamJihyun99 <namjihyun9936@gmail.com>
2025-09-15 17:47:12 +0900

    del: 임베딩 모델 제거

--- app/end/__init__.py
+++ app/end/__init__.py
@@ -1,12 +1,10 @@
 from .stock_router import router as stock_router
 from .websocket_router import router as websocket_router
 from .backtest_router import router as backtest_router
-from .embedding_router import router as embedding_router
 
 __all__ = [
     "stock_router",
     "websocket_router",
     "backtest_router",
-    "embedding_router",
 ]
 
